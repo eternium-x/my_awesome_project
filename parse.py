8077793010dcf793773219599163c16edd1e3ccc
eternium-x <63191043+eternium-x@users.noreply.github.com>
2020-04-05 17:10:58 +0530

    parse things
    parses

---
+++ parse.py
@@ -0,0 +1,23 @@
+import argparse
+
+def add(a, b):
+    return a+b
+
+def sub(a, b):
+    return a-b
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-o', '--output', action='store_true', help='shows_output')
+
+##args = parser.parse_args()
+####print(args)
+##if args.output:
+##    print('nonsense')
+
+##parser.add_argument('--name', required=True)
+
+parser.add_argument('name')
+parser.add_argument('age')
+args = parser.parse_args()
+print(f'Hello {args.name}, you are {args.age}')
